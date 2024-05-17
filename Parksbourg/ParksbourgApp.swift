3424da907706f706cad9a3ee105f65760d9aa69b
Leon <wirowik67@gmail.com>
2024-05-17 15:07:55 +0200

    Initial Commit

---
+++ Parksbourg/ParksbourgApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParksbourgApp.swift
+//  Parksbourg
+//
+//  Created by Leon Le Berre on 5/17/24.
+//
+
+import SwiftUI
+
+@main
+struct ParksbourgApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
